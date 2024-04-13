7b2df0edb939c936251682f2b5c239a41ac8cb89
Gumball <haroldortiz@outlook.es>
2024-04-13 17:27:02 -0500

    Avanze del proyecto

--- pagina.js
+++ pagina.js
@@ -1,1 +1,18 @@
-window.alert("Hola mundo");
+var nombreInput = document.getElementById('nombreUsuario');
+var contraseñaInput= document.getElementById('contraseñaUsuario');
+var registrarBtn = document.getElementById('accederBtn');
+
+accederBtn.addEventListener('click',function(){
+    var nombre = nombreInput.value;
+    var contraseña = contraseñaInput.value;
+
+    nombreInput.value = '';
+    contraseñaInput.value = '';
+    });
+
+//Crear Variables del registro.html cada uno de los formularios e ingresarlos con el evento 
+//@willy y @lily en uds confio basuras <3 y agregar funcion de limpiar cuadro
+
+registrarBtn.addEventListener('click',function(){
+
+    });
